f8ae320ef68601b31c8cc8ebd126fae133bed593
sshojiro <shojiro.shibayama@gmail.com>
2019-04-05 17:17:37 +0900

    feat: added mml.svm.SVR. The class uses matlab built-in SVR function.

---
+++ +mml/+svm/SVR.m
@@ -0,0 +1,47 @@
+classdef SVR < mml.base.BaseEstimator & mml.base.BaseRegressor
+    properties(SetAccess=public, GetAccess=public)
+        cost
+        epsilon
+        gamma
+        kernel = 'gaussian'
+        model
+    end
+    methods(Access=public)
+        function self = SVR(cost, epsilon, gamma, kernel)
+            %% mml.svm.SVR(cost, epsilon, gamma)
+            %
+            % cost: regularization coefficients
+            % epsilon: epsilon-tube width
+            % gamma: Gaussian kernel
+            % kernel: type of kernel function
+            %     gaussian, rbf, linear, polynomial
+            if ~exist('cost', 'var'),cost=2;end
+            if ~exist('epsilon', 'var'),epsilon=2;end
+            if ~exist('gamma', 'var'),gamma=2;end
+            if ~exist('kernel', 'var'), kernel='gaussian';end
+            assert(any(strcmp(kernel, {'gaussian', 'rbf', 'linear', 'polynomial'})),...
+                'Kernel function must be either gaussian, rbf, linear, or polynomial')
+            self.cost = cost;
+            self.epsilon = epsilon;
+            self.gamma = gamma;
+            kernel = lower(kernel);
+        end
+        function self = fit(self, x, y)
+            self.model = fitrsvm(x,y,'Standardize',true,...
+                'KernelFunction', self.kernel,...
+                'BoxConstraint', self.cost,...
+                'Epsilon', self.epsilon,...
+                'KernelScale',1/sqrt(self.gamma));
+        end
+        function y = predict(self, x)
+            y = predict(self.model, x);
+        end
+        function proba = predictProba(self, x)
+          [~, score] = predict(self.model, x);
+          proba = score(:, 2);
+        end
+        function val = score(self, x, y)
+            val = mml.metrics.r2score(y, self.model.predict(x));
+        end
+    end
+end
