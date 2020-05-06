b93f6eda485e158d49df4d362fcb50e341d5d094
sshojiro <shojiro.shibayama@gmail.com>
2020-05-07 00:45:05 +0900

    feat: added DotProduct kernel

---
+++ +mml/+gaussian_process/+kernels/DotProduct.m
@@ -0,0 +1,50 @@
+classdef DotProduct  < mml.gaussian_process.kernels.Kernel
+    properties
+        sigma0
+    end
+    methods
+        function self = DotProduct(sigma0)
+            if nargin > 0
+                if~exist('sigma0','var'),sigma0 = 1;end
+                self.sigma0 = sigma0;
+            end
+        end
+        function varargout = subsref(self, s)
+            if length({s.type})==2 && isequal({s.type}, {'.' '()'})
+                % k.kernel(X, Y) 
+                % s.subs 
+                %   => 'kernel'
+                %   => {X, Y}
+                [name,XY]=s.subs;
+                X=XY{1};
+                if length(XY)==2,Y=XY{end};end
+                varargout = {self.(name)(X,Y)};
+                return
+            end
+            switch s.type
+                case '()'
+                    if length(s.subs)==1
+                        X = s.subs{1};
+                        K = squareform(pdist(X, @self.kernel));
+                        nSamples = size(K,1);
+                        K = K + arrayfun(@(i)X(i,:)*X(i,:)',1:nSamples);
+                        varargout = { K };
+                    elseif length(s.subs)==2
+                        [X,Y] = s.subs{:};
+                        varargout = {cdist(X,Y,@self.kernel)};
+                    else
+                        error('the number of input: %d',length(s.subs));
+                    end
+                case '.' 
+                    if ischar(s.subs)
+                        varargout = {self.(s.subs)};
+                    else
+                        varargout = {self.kernel(self.(s.subs{:}))};
+                    end
+            end
+        end
+        function ret = kernel(self,x,y)
+            ret = y * x';
+        end
+    end
+end
--- +mml/+gaussian_process/+kernels/Kernel.m
+++ +mml/+gaussian_process/+kernels/Kernel.m
@@ -1,11 +1,11 @@
 classdef Kernel
     properties
-        kernel
+%         kernel
     end
     methods
-        function self = Kernel(kernel)
-            if nargin==0, kernel=@(x,y)x'*y; end
-            self.kernel = kernel;
+        function self = Kernel()
+%             if nargin==0, kernel=@(x,y)x'*y; end
+%             self.kernel = kernel;
         end
         %https://jp.mathworks.com/help/matlab/matlab_oop/create-a-simple-class.html#buo0ann
         function objnew = plus(obj1, obj2)
@@ -21,11 +21,18 @@ classdef Kernel
 %             objnew = mml.gaussian_process.kernels.Kernel( k );
 %         end
         function varargout = subsref(self, s)
-            if~strcmp(s.type, '()')
-                error('class input is referred by Class()')
+            switch s.type
+                case '()'
+                    varargout = {self.kernel(s.subs{:})};
+                case '.'
+                    if ischar(s.subs)
+                        varargout = {self.(s.subs)};
+                    elseif iscell(s.subs)
+                        varargout = cellfun(@(ch)self.(ch), s.subs);
+                    end
+                otherwise
+                    error('class input is referred by Class()')
             end
-            varargout = {self.kernel(s.subs{:})};
         end
-
     end
 end
